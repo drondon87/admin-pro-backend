c7c7a4021001eb1a982ab75aa2112e48c140616c
drondon87 <domingorondon87@gmail.com>
2020-12-08 19:26:50 +0100

    Fin sección 13

--- controllers/medico.controller.js
+++ controllers/medico.controller.js
@@ -35,18 +35,71 @@ const crearMedico = async(req, res = response) => {
     }
 }
 
-const actualizarMedico = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'Actualizar Medicos'
-    })
+const actualizarMedico = async(req, res = response) => {
+
+    const { id } = req.params;
+    const uid = req.uid;
+
+    try {
+
+        const medicoDB = await Medico.findById(id);
+
+        if (!medicoDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe un medico por ese id'
+            })
+        }
+
+        const cambiosMedico = {
+            ...req.body,
+            usuario: uid
+        }
+
+        const medicoActualizado = await Medico.findByIdAndUpdate(id, cambiosMedico, { new: true });
+
+        res.json({
+            ok: true,
+            msg: 'Medico Actualizado',
+            hospital: medicoActualizado
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
 }
 
-const borrarMedico = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'Borrar Medicos'
-    })
+const borrarMedico = async(req, res = response) => {
+    const { id } = req.params;
+
+    try {
+
+        const medicoDB = await Medico.findById(id);
+
+        if (!medicoDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'No existe un medico por ese id'
+            })
+        }
+
+        await Medico.findByIdAndDelete(id);
+
+        res.json({
+            ok: true,
+            msg: 'Medico Eliminado'
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado ... revisar logs'
+        })
+    }
 }
 
 module.exports = {
--- routes/medico.routes.js
+++ routes/medico.routes.js
@@ -20,7 +20,12 @@ router.post('/', [
     ],
     crearMedico);
 
-router.put('/:id', [], actualizarMedico);
+router.put('/:id', [
+    validarJWT,
+    check('nombre', 'El nombre del medico es necesario').not().isEmpty(),
+    check('hospital', 'El hospital id debe de ser valido').isMongoId(),
+    validarCampos
+], actualizarMedico);
 
 router.delete('/:id', validarJWT, borrarMedico);
 
