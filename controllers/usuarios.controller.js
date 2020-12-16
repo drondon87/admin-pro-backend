4b16df4faa0e30669fe2e7418844d789fc6ed985
drondon87 <domingorondon87@gmail.com>
2020-12-16 19:11:50 +0100

    Se agrega validacion de email de google

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -89,7 +89,14 @@ const actualizarUsuario = async(req, res = response) => {
             }
         }
 
-        campos.email = email;
+        if (!usuarioDB.google) {
+            campos.email = email;
+        } else if (usuarioDB.email !== email) {
+            res.status(400).json({
+                ok: false,
+                msg: 'Usuario de google no puede cambiar su correo'
+            })
+        }
         const usuarioActualizado = await Usuario.findByIdAndUpdate(uid, campos, { new: true });
 
         res.json({
